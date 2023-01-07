24eba4265735dd056aaebd5e9a0995d33c3137da
ratslove <122135063+ratslove@users.noreply.github.com>
2023-01-06 20:58:42 -0500

    Update thing2.lua

--- thing2.lua
+++ thing2.lua
@@ -351,10 +351,19 @@ end)
             
             States.InfStamina = true
             
-            
-            end
-        end
-end)
+            local plr = game:GetService("Players").LocalPlayer
+                        for i,v in next, getgc() do 
+                            if type(v) == "function" and getfenv(v).script and getfenv(v).script == plr.Character.ClientInputHandler then 
+                                for i2,v2 in next, debug.getupvalues(v) do 
+                                    if type(v2) == "number" then 
+                                        debug.setupvalue(v, i2, math.huge)
+                                    end
+                                end
+                            end
+                        end                        
+                    end
+                end
+    end)
 
 coroutine.wrap(function()
 	while wait() do
